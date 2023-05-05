25d4de09f269c3f7b09eeecc4713869feef80dbe
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2023-05-04 22:52:18 -0600

    🎨 improve get user products logic

--- stored-procedures/get_user_products.sql
+++ stored-procedures/get_user_products.sql
@@ -1,11 +1,14 @@
 DELIMITER //
 CREATE PROCEDURE `get_user_products` (
-	in user_id INT
+	in catalog_name VARCHAR(110)
 )
   BEGIN
+	DECLARE user_id VARCHAR(110);
 
-    SELECT `id`, `name`, `description`, `price`, `image`, `inventory_quantity`
-    FROM `products`
-    WHERE `products`.`user_id` = user_id ;
+  SELECT `id` INTO user_id FROM `users` WHERE `users`.`catalog_name` = catalog_name;
+
+  SELECT `id`, `name`, `description`, `price`, `image`, `inventory_quantity`
+  FROM `products`
+  WHERE `products`.`user_id` = user_id ;
 
   END // DELIMITER ;
